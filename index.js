5fa65549180c7384b8250c3fcfc0d2bd08b4bca7
chris-schmitz <schmitz.chris@gmail.com>
2017-05-14 20:35:07 -0500

    Adds socket test

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 server = express()
+const http = require('http').Server(server)
+const io = require('socket.io')(http)
+
 PORT = process.env.PORT
 
 server.get('/', (request, response) => {
     response.json({test: 'worked'})
 })
+
+function hub(socket){
+    console.log('user connected')
+    console.log('user connected')
+
+    socket.on('login', (request, ...args) => {
+        console.log('logged in')
+        console.log(`request: ${request}`)
+        console.log(`args: ${args}`)
+        socket.emit('response', "you're logged in", "welcome!")
+    })
+}
+
+io.on('connection', hub)
 
 server.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
