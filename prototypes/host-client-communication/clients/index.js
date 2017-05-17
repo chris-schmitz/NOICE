708fa597c0857422f9935a4554ba19b78795bc31
chris-schmitz <schmitz.chris@gmail.com>
2017-05-16 20:28:27 -0500

    adds listener for firing the pattern

--- prototypes/host-client-communication/clients/index.js
+++ prototypes/host-client-communication/clients/index.js
@@ -1,7 +1,17 @@
 const io = require('socket.io-client')
+const {exec} = require('child_process')
 
 console.log('connecting')
-const socket = io.connect('http://localhost:3002')
+const socket = io.connect('https://stormy-sierra-95940.herokuapp.com/')
+
+function firePattern (type) {
+    // a bit goofey at the moment, but this is a way I can get a poc without
+    // having to refactor the pattern prototype.
+    const command = 'node ../../noice-patterns/index.js'
+    exec(command, (error, stdout, stderr) => {
+        console.log({error, stdout, stderr})
+    })
+}
 
 socket.on('connect', () => {
     console.log('connected')
@@ -12,5 +22,7 @@ socket.on('connect', () => {
         console.log(args)
     })
 })
+
+socket.on('connect_error', error => console.error(error))
 
 // update and fire
